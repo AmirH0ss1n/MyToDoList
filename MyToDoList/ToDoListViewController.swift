72aec97f5ad957eecafc411196393b71798491c7
AmirH0sse1n <hasheminasr1@gmail.com>
2021-05-09 10:10:55 +0430

    Add functionality complete.

--- MyToDoList/ToDoListViewController.swift
+++ MyToDoList/ToDoListViewController.swift
@@ -9,44 +9,72 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemcArray = ["hi","hello","hola"]
-
+    var itemArray = ["hi","hello","hola"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    //MARK - tableView Datasource methods
- 
+//MARK - tableView Datasource methods
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemcArray.count
+        return itemArray.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoitemCell", for: indexPath)
-        cell.textLabel?.text = itemcArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
-      
-    //MARK - tableView delegate methods
+    
+//MARK - tableView delegate methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemcArray[indexPath.row])
+        print(itemArray[indexPath.row])
         
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
             tableView.cellForRow(at: indexPath)?.accessoryType = .disclosureIndicator
-
+            
         }else{
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
+    
+    
+//MARK - Bar Button Item (Add New Item)
+    
+    
+    
+    @IBAction func addbuttonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New ToDo Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "add item", style: .default) { (action) in
+            
+            //what while happen after user tap on the add button
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Craat new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
